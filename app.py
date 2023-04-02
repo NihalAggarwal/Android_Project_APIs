5b4809b82600baac133d93a0ebfd5bfe10f22ac3
Nihal Aggarwal <84188658+NihalAggarwal@users.noreply.github.com>
2023-04-02 15:01:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,35 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/predict_loan')
+def predict():
+    Gender = request.form.get('Gender')
+    Married = request.form.get('Married')
+    Dependents = request.form.get('Dependents')
+    Education = request.form.get('Education')
+    Self_Employed = request.form.get('Self_Employed')
+    ApplicantIncome = request.form.get('ApplicantIncome')
+    CoapplicantIncome = request.form.get('CoapplicantIncome')
+    LoanAmount = request.form.get('LoanAmount')
+    Loan_Amount_Term = request.form.get('Loan_Amount_Term')
+    Credit_History = request.form.get('Credit_History')
+    Property_Area = request.form.get('Property_Area')
+
+    # result = {'Gender': Gender, 'Married': Married, 'Dependents': Dependents, 'Education': Education,
+    #           'Self_Employed': Self_Employed, 'ApplicantIncome': ApplicantIncome,
+    #           'CoapplicantIncome': CoapplicantIncome, 'LoanAmount': LoanAmount, 'Loan_Amount_Term': Loan_Amount_Term,
+    #           'Credit_History': Credit_History, 'Property_Area': Property_Area}
+
+    input_query = np.array([[Gender, Married, Dependents, Education, Self_Employed, ApplicantIncome, CoapplicantIncome,
+                             LoanAmount, Loan_Amount_Term, Credit_History, Property_Area]])
+
+    var = model.predict(input_query)[0]
+
+    if (str(var) == "1"):
+
+        return jsonify({'Loan': "Passed"})
+    else:
+        return jsonify({'Loan': "Failed"})
+
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0')
